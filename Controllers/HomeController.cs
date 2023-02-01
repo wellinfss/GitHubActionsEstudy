4b8dce0a4bd2789cfc336893d8f36f1be4bc000c
Wellington F. S. Silva <wellinfss@gmail.com>
2023-02-01 07:58:07 -0300

    Alter to Push

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,9 @@ namespace NetCoreGitHubActionsOnPush.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public void Push(){
+
+        }
     }
 }
